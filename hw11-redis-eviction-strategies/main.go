6765ee7a519d79f60f47dcfc710861140d6ce6cb
Alex Ovsianicoff <o.ovsiannykov@rozetkapay.com>
2025-02-08 15:08:26 +0200

    Enhance Redis configuration and implement keyspace notifications; remove obsolete info file and adjust preload key logic

--- hw11-redis-eviction-strategies/main.go
+++ hw11-redis-eviction-strategies/main.go
@@ -17,11 +17,12 @@ import (
 )
 
 var (
-	rdb          *redis.ClusterClient
 	locks        = sync.Map{}
 	expireJitter = 10 // Jitter in seconds for early expiration
 	redisClient  *redis.ClusterClient
 	ctx          = context.Background()
+
+	defultKeysTTL = time.Minute * 10
 )
 
 func main() {
@@ -34,10 +35,9 @@ func main() {
 		Addrs: redisNodes,
 	})
 
-	preloadKeys()
-
 	r := mux.NewRouter()
 
+	r.HandleFunc("/preloadKeys", func(w http.ResponseWriter, r *http.Request) { preloadKeys() })
 	r.HandleFunc("/evict/lru", func(w http.ResponseWriter, r *http.Request) { testEviction(w, "allkeys-lru") })
 	r.HandleFunc("/evict/lfu", func(w http.ResponseWriter, r *http.Request) { testEviction(w, "allkeys-lfu") })
 	r.HandleFunc("/evict/random", func(w http.ResponseWriter, r *http.Request) { testEviction(w, "allkeys-random") })
@@ -49,8 +49,24 @@ func main() {
 	r.HandleFunc("/fetch/probabilistic/{key}", fetchWithProbabilisticExpiration).Methods("GET")
 	r.HandleFunc("/set/{key}/{value}/{ttl}", setCacheValue).Methods("POST")
 
+	go monitorEvictions()
+
 	log.Println("Server running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func monitorEvictions() {
+	err := redisClient.ConfigSet(ctx, "notify-keyspace-events", "KEA").Err()
+	if err != nil {
+		log.Fatalf("Failed to enable keyspace notifications: %v", err)
+	}
+
+	pubsub := redisClient.PSubscribe(ctx, "__keyevent@0__:evicted")
+	defer pubsub.Close()
+
+	for msg := range pubsub.Channel() {
+		log.Printf("Evicted key: %s", msg.Payload)
+	}
 }
 
 func getRedisClusterNodes() []string {
@@ -62,28 +78,20 @@ func getRedisClusterNodes() []string {
 }
 
 func preloadKeys() {
-	for i := 0; i < 25000; i++ {
+	for i := 0; i < 10000; i++ {
 		key := fmt.Sprintf("key:%d", i)
-		value := rand.Intn(1000)
-		ttl := (time.Duration(rand.Intn(3)) + 1) * time.Minute // Random 1 min < TTL <= 4 min
-		if err := redisClient.Set(ctx, key, value, ttl).Err(); err != nil {
+		value := fmt.Sprintf("V%d", i)
+		if err := redisClient.Set(ctx, key, value, defultKeysTTL).Err(); err != nil {
 			log.Printf("Failed to set key %s: %v", key, err)
 			return
 		}
+		log.Printf("Set key %s: %v", key, value)
 	}
-	log.Println("Preloaded 25000 keys into Redis Cluster.")
+	log.Println("Preloaded 10000 keys into Redis Cluster.")
 }
 
 func testEviction(w http.ResponseWriter, policy string) {
-	err := redisClient.ConfigSet(ctx, "maxmemory-policy", policy).Err()
-	if err != nil {
-		log.Printf("Failed to set ConfigSet eviction policy %s: %v", policy, err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("Failed to set eviction policy: %v", err)))
-		return
-	}
-
-	err = redisClient.Do(ctx, "CONFIG", "SET", "maxmemory-policy", policy).Err()
+	err := redisClient.Do(ctx, "CONFIG", "SET", "maxmemory-policy", policy).Err()
 	if err != nil {
 		log.Printf("Failed to set Do eviction policy %s: %v", policy, err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -94,27 +102,24 @@ func testEviction(w http.ResponseWriter, policy string) {
 	log.Printf("Set eviction policy to: %s", policy)
 
 	for i := 0; i < 10000; i++ {
-		key := fmt.Sprintf("test-key:%d", i)
-		time.Sleep(time.Millisecond * 5)
-		if err := redisClient.Set(ctx, key, i, 0).Err(); err != nil {
+		key := fmt.Sprintf("test-key:%d-%s", i, policy)
+		time.Sleep(time.Millisecond)
+		if err := redisClient.Set(ctx, key, i, defultKeysTTL).Err(); err != nil {
 			log.Printf("Failed to set key %s: %v", key, err)
-			return
+			time.Sleep(time.Second)
+			continue
 		}
 	}
 
 	w.Write([]byte(fmt.Sprintf("Tested eviction strategy: %s", policy)))
 }
 
-func init() {
-	rdb = redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"localhost:6379"},
-	})
-}
-
 func mockDBQuery(key string) (string, error) {
 	time.Sleep(500 * time.Millisecond) // Simulating DB latency
 	return "value_from_db_" + key, nil
 }
+
+var ttlSec int
 
 // Method 1: Directly fetching from DB
 func fetchFromDB(w http.ResponseWriter, r *http.Request) {
@@ -125,7 +130,7 @@ func fetchFromDB(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "DB error", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(value))
+	w.Write([]byte(fmt.Sprintf("fetchFromDB: key:%s -> value: %s", key, value)))
 }
 
 // Method 2: Fetch from cache, fallback to DB on miss
@@ -133,17 +138,19 @@ func fetchWithCache(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
-	val, err := rdb.Get(ctx, key).Result()
+	val, err := redisClient.Get(ctx, key).Result()
 	if err == redis.Nil {
 		val, err = mockDBQuery(key)
+		log.Printf("made DBQuery %s: %v", key, val)
+
 		if err == nil {
-			rdb.Set(ctx, key, val, 10*time.Second)
+			redisClient.Set(ctx, key, val, time.Duration(ttlSec)*time.Second)
 		}
 	} else if err != nil {
 		http.Error(w, "Cache error", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(val))
+	w.Write([]byte(fmt.Sprintf("fetchWithCache: key:%s -> value: %s", key, val)))
 }
 
 // Method 3: External recomputation with locking
@@ -151,7 +158,7 @@ func fetchWithExternalRecomputation(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
-	if val, err := rdb.Get(ctx, key).Result(); err == nil {
+	if val, err := redisClient.Get(ctx, key).Result(); err == nil {
 		w.Write([]byte(val))
 		return
 	}
@@ -162,7 +169,7 @@ func fetchWithExternalRecomputation(w http.ResponseWriter, r *http.Request) {
 	defer lock.Unlock()
 
 	// Double check after acquiring lock
-	if val, err := rdb.Get(ctx, key).Result(); err == nil {
+	if val, err := redisClient.Get(ctx, key).Result(); err == nil {
 		w.Write([]byte(val))
 		return
 	}
@@ -172,9 +179,10 @@ func fetchWithExternalRecomputation(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "DB error", http.StatusInternalServerError)
 		return
 	}
+	log.Printf("made DBQuery %s: %v", key, val)
 
-	rdb.Set(ctx, key, val, 10*time.Second)
-	w.Write([]byte(val))
+	redisClient.Set(ctx, key, val, time.Duration(ttlSec)*time.Second)
+	w.Write([]byte(fmt.Sprintf("fetchWithExternalRecomputation: key:%s -> value: %s", key, val)))
 }
 
 // Method 4: Probabilistic early expiration
@@ -182,21 +190,23 @@ func fetchWithProbabilisticExpiration(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
-	val, err := rdb.Get(ctx, key).Result()
+	val, err := redisClient.Get(ctx, key).Result()
 	if err == redis.Nil || shouldRecompute(key) {
 		val, err = mockDBQuery(key)
 		if err == nil {
-			rdb.Set(ctx, key, val, time.Duration(10+rand.Intn(expireJitter))*time.Second)
+			redisClient.Set(ctx, key, val, time.Duration(rand.Intn(expireJitter))*time.Second)
 		}
+		log.Printf("made DBQuery %s: %v", key, val)
 	} else if err != nil {
 		http.Error(w, "Cache error", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(val))
+	w.Write([]byte(fmt.Sprintf("fetchWithProbabilisticExpiration: key:%s -> value: %s", key, val)))
+
 }
 
 func shouldRecompute(key string) bool {
-	ttl, err := rdb.TTL(ctx, key).Result()
+	ttl, err := redisClient.TTL(ctx, key).Result()
 	if err != nil || ttl <= 0 {
 		return true
 	}
@@ -209,7 +219,7 @@ func setCacheValue(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 	value := vars["value"]
-	ttlSec, _ := strconv.Atoi(vars["ttl"])
-	rdb.Set(ctx, key, value, time.Duration(ttlSec)*time.Second)
-	w.Write([]byte("OK"))
+	ttlSec, _ = strconv.Atoi(vars["ttl"])
+	redisClient.Set(ctx, key, value, time.Duration(ttlSec)*time.Second)
+	w.Write([]byte(fmt.Sprintf("setCacheValue: key:%s -> value: %s. Ok", key, value)))
 }
