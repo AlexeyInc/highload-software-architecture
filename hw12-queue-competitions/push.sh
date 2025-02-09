1a7b5e2abff3bb093a2cdcac3b274cec7ea6afa8
Alex Ovsianicoff <o.ovsiannykov@rozetkapay.com>
2025-02-09 13:59:59 +0200

    Added base config files

---
+++ hw12-queue-competitions/push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# RabbitMQ Push
+RABBITMQ_USER="user"
+RABBITMQ_PASS="password"
+RABBITMQ_QUEUE="test_queue"
+RABBITMQ_EXCHANGE=""
+MESSAGE="Hello from Bash"
+
+echo "Publishing message to RabbitMQ..."
+curl -u $RABBITMQ_USER:$RABBITMQ_PASS -X POST -H "Content-Type: application/json" \
+  -d '{"properties":{}, "routing_key":"'$RABBITMQ_QUEUE'", "payload":"'$MESSAGE'", "payload_encoding":"string"}' \
+  http://localhost:15672/api/exchanges/%2F/amq.default/publish
+
+echo "Publishing message to Redis RDB..."
+redis-cli -h localhost -p 6379 XADD stream_rdb * message "$MESSAGE"
+
+echo "Publishing message to Redis AOF..."
+redis-cli -h localhost -p 6380 XADD stream_aof * message "$MESSAGE"
+
+echo "Message published successfully!"
+
+# Pull script to retrieve messages
+
+echo "Consuming message from RabbitMQ..."
+curl -u $RABBITMQ_USER:$RABBITMQ_PASS -X GET http://localhost:15672/api/queues/%2F/$RABBITMQ_QUEUE/get -H "Content-Type: application/json" -d '{"count":1, "ackmode":"ack_requeue_false", "encoding":"auto"}'
+
+echo "Consuming message from Redis RDB..."
+redis-cli -h localhost -p 6379 XREAD COUNT 1 STREAMS stream_rdb 0
+
+echo "Consuming message from Redis AOF..."
+redis-cli -h localhost -p 6380 XREAD COUNT 1 STREAMS stream_aof 0
+
+echo "Message consumption completed!"
